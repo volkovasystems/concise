ad23c8d1bd7c9f79cdd0f3906252d215b024685f
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-30 07:30:30 +0800

    Initial commit

---
+++ component.js
@@ -0,0 +1,22 @@
+define( "component", 
+	[ 
+		"angular", 
+		"underscore"
+	],
+	function(  ){
+		function Component( ){
+
+		}
+
+		Component.prototype.integrate = function integrate( ){
+
+		};
+
+		Component.prototype.bootstrap = function bootstrap( ){
+
+		};
+
+		Component.prototype.build = function build( ){
+			
+		}
+	} );
